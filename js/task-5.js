d12455994281672c70047b9115f5071809f8fc0e
Alena Kodola <alenakodola@gmail.com>
2025-01-15 23:37:53 +0200

    rename file css

--- js/task-5.js
+++ js/task-5.js
@@ -1,14 +1,13 @@
 function getRandomHexColor() {
-    return `#${Math.floor(Math.random() * 16777215)
-      .toString(16)
-      .padStart(6, 0)}`;
-  }
-  const changeBodyColor = document.querySelector('.change-color');
-  const textBodyColor = document.querySelector('.color');
-  const body = document.querySelector('body');
- changeBodyColor.addEventListener("click", () => {
-  const randomColor = getRandomHexColor()
-    body.style.backgroundColor = randomColor;
-    textBodyColor.textContent = `${randomColor}`
-  })
- 
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+const changeBodyColor = document.querySelector(".change-color");
+const textBodyColor = document.querySelector(".color");
+const body = document.querySelector("body");
+changeBodyColor.addEventListener("click", () => {
+  const randomColor = getRandomHexColor();
+  body.style.backgroundColor = randomColor;
+  textBodyColor.textContent = `${randomColor}`;
+});
